c1c191b874977830d5f793c6a0130b0ae54a8ffe
Temirgaliyev Yelmurat <temirgalievelmurat@gmail.com>
2020-03-16 03:19:13 +0600

    Replaced files for deploy

--- anti_corona/constants.py
+++
@@ -1,3 +0,0 @@
-attributes = ['entry_date', 'flight', 'fullname', 'id_number', 
-'birth_date', 'passport_number', 'citizenship', 'phone', 'before_arrival',
-'region', 'residence', 'work_place', 'found', 'hospitalized', 'hospital']
